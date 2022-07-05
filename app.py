f162980f792c280f661f8f0cb02331251d23e9ed
sanjiv <sanjivgautamofficial@gmail.com>
2022-07-05 11:34:37 +0545

    file name check

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask import jsonify
 # from caption import load_model,get_final_output
-import filetype
 from neuraltalk import FeatureExtractor
 from caption import get_model,get_captions
 from flask_sqlalchemy import SQLAlchemy
@@ -9,7 +8,7 @@ from PIL import Image
 import imagehash
 import random 
 from gpt3 import generate_caption,generate_prompt
-from utils import CAPTION_CATEGORIES
+from utils import CAPTION_CATEGORIES,check_image
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -32,11 +31,7 @@ MAX_LENGTH = 5
 feature_extractor = FeatureExtractor()
 model = get_model()
 
-def check_image(request):
-    f = request.files.get('image')
-    if f and filetype.is_image(f):
-        return True 
-    return False
+
 
 @app.route("/",methods=['GET', 'POST'])
 def home():
@@ -71,13 +66,13 @@ def home():
             random_caption = random.choice(captions)
             image_caption= AIImageCaption.query.filter_by(caption=random_caption,category=category).first()
             if image_caption:
-                ai_generated_captions = image_caption.gen_caption.split("//")
+                ai_generated_captions = image_caption.gen_caption.split("<>")
                 return jsonify({"caption":ai_generated_captions})
 
             prompt = generate_prompt(random_caption,category)
             ai_generated_caption = generate_caption(prompt)
             ai_generated_caption = [caption.text.strip() for caption in ai_generated_caption.choices]
-            parsed_ai_generated_caption = "//".join(ai_generated_caption)
+            parsed_ai_generated_caption = "<>".join(ai_generated_caption)
             
             ai_captions = AIImageCaption(category=category,caption=random_caption,gen_caption=parsed_ai_generated_caption)
             db.session.add(ai_captions)
--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
+from PIL import Image
+
 CAPTION_CATEGORIES = ["happy","inspiring","sad","motivation","sarcastic","mind-blowing","real life"]
+
+def check_image(request):
+    f = request.files.get('image')
+    try:
+        img = Image.open(f)
+        w,h = img.size
+        if w>50 and h>50:
+            return True
+        return False 
+    except IOError:
+        return False
